7f06218a7b5c4f78dde1028200f5df36d69bea36
code8sam <work.social.satyam@gmail.com>
2023-07-04 05:08:04 +0530

    optimised approach for intersection of 2 arrays

--- src/Arrays/IntersectionOfArrays.java
+++ src/Arrays/IntersectionOfArrays.java
@@ -3,13 +3,16 @@ package Arrays;
 import java.util.ArrayList;
 
 public class IntersectionOfArrays {
+    /*
+
+    ----------------------------BRUTE FORCE APPROACH----------------------------------------------
     public static void main(String[] args) {
 
         int [] arr1 = {1,2,2,1};
         int [] arr2 = {2,2};
 //        int [] arr1 = {10, 19, 20, 30, 40, 40, 40, 50};
 //        int [] arr2 = {15, 16, 17, 18, 20, 25, 30, 30, 40};
-        
+
         ArrayList<Integer> intersec = new ArrayList<>();
 
         for(int i=0; i<arr1.length; i++){
@@ -28,5 +31,35 @@ public class IntersectionOfArrays {
             }
         }
         return present;
+    }
+    */
+    public static void main(String[] args) {
+
+//        -----------------------------------OPTIMISED APPROACH------------------------------------------
+
+        int [] arr1 = {10, 19, 20, 30, 40, 40, 40, 50};
+        int [] arr2 = {15, 16, 17, 18, 20, 25, 30, 30, 40};
+
+        System.out.println(intersection(arr1, arr2));
+
+    }
+
+    private static ArrayList<Integer> intersection(int[] arr1, int[] arr2) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int i=0;
+        int j=0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<arr2[j]){
+                i++;
+            }else if(arr1[i]>arr2[j]){
+                j++;
+            }else{
+                ans.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        return ans;
     }
 }
