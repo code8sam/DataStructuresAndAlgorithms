10fd53ec1013621ee43611c6be32735c4dfa6367
code8sam <work.social.satyam@gmail.com>
2023-07-07 19:27:29 +0530

    optimised approach : running sum of 1d array

--- src/Arrays/RunningSumOf1DArray.java
+++ src/Arrays/RunningSumOf1DArray.java
@@ -1,29 +1,48 @@
 package Arrays;
 
 public class RunningSumOf1DArray {
+
+
+//    public static void main(String[] args) {
+//
+//        // BRUTE FORCE APPROACH
+//        int arr [] = {1, 2, 3, 4};
+//        int result [] = runningSum(arr);
+//        for (int element : result) {
+//            System.out.print(element+" ");
+//        }
+//    }
+//    private static int[] runningSum(int[] nums) {
+//        int ans [] = new int[nums.length];
+//        for(int i=0; i<nums.length; i++){
+//            ans[i] = runningSumAtIndex(nums, i);
+//        }
+//        return ans;
+//    }
+//    private static int runningSumAtIndex(int[] nums, int i) {
+//        int sum = 0;
+//        for(int x=0; x<=i; x++){
+//            sum+=nums[x];
+//        }
+//        return sum;
+//    }
+
     public static void main(String[] args) {
-        int arr [] = {1, 2, 3, 4};
-        int result [] = runningSum(arr);
+        // OPTIMISED APPROACH
+
+        int arr[] = {1, 2, 3, 4};
+        int result[] = runningSum(arr);
         for (int element : result) {
-            System.out.print(element+" ");
+            System.out.print(element + " ");
         }
     }
 
     private static int[] runningSum(int[] nums) {
-
-        int ans [] = new int[nums.length];
-
-        for(int i=0; i<nums.length; i++){
-            ans[i] = runningSumAtIndex(nums, i);
+        int result [] = new int[nums.length];
+        result[0] = nums[0];
+        for(int i=1; i<nums.length; i++){
+            result[i] = result[i-1]+nums[i];
         }
-        return ans;
-    }
-
-    private static int runningSumAtIndex(int[] nums, int i) {
-        int sum = 0;
-        for(int x=0; x<=i; x++){
-            sum+=nums[x];
-        }
-        return sum;
+        return result;
     }
 }
