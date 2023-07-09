58478338edc2ec5e5a7ea635b421189462c9d24a
code8sam <work.social.satyam@gmail.com>
2023-07-09 17:19:13 +0530

    wave display of an array : horizontal

---
+++ src/Arrays/WaveDisplayHorizontal.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class WaveDisplayHorizontal {
+    public static void main(String[]args){
+        int [][] given = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+
+        for(int i=0; i<given.length; i++){
+            if(i%2==0){
+                for(int j=0; j<given[0].length; j++){
+                    System.out.print(given[i][j]+" ");
+                }
+            }else {
+                for(int j=given[0].length-1; j>=0; j--){
+                    System.out.print(given[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
