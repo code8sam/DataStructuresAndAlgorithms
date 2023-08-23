f415452571065cf2189d8dfe603b5e0dd54c7294
code8sam <work.social.satyam@gmail.com>
2023-08-23 19:49:53 +0530

    leetcode 151 : reverse words in a string

---
+++ src/StringAndStringBuilder/ReverseWordsInAString.java
@@ -0,0 +1,26 @@
+package StringAndStringBuilder;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        String given = "  hello      world  ";
+        System.out.println(reverseWords(given));
+    }
+
+    private static String reverseWords(String s) {
+        // remove leading and trailing spaces
+        s = s.trim();
+        // split -> divides the string and puts
+        // elements into array based
+        // on delimiter condition
+        // \s+ is a regular expression for one or more spaces
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        // reverses the elements of the list
+        Collections.reverse(wordList);
+        // returns a string separated by a space
+        return String.join(" ", wordList);
+    }
+}
