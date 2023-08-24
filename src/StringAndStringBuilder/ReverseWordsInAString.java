65a210bb4621c96be480812b340bac72f149733b
code8sam <work.social.satyam@gmail.com>
2023-08-25 01:49:21 +0530

    alternate approach to : reverse words in a string

--- src/StringAndStringBuilder/ReverseWordsInAString.java
+++ src/StringAndStringBuilder/ReverseWordsInAString.java
@@ -9,18 +9,32 @@ public class ReverseWordsInAString {
         String given = "  hello      world  ";
         System.out.println(reverseWords(given));
     }
-
-    private static String reverseWords(String s) {
-        // remove leading and trailing spaces
-        s = s.trim();
-        // split -> divides the string and puts
-        // elements into array based
-        // on delimiter condition
-        // \s+ is a regular expression for one or more spaces
-        List<String> wordList = Arrays.asList(s.split("\\s+"));
-        // reverses the elements of the list
-        Collections.reverse(wordList);
-        // returns a string separated by a space
-        return String.join(" ", wordList);
+    private static String reverseWords(String s){
+        s = s.replaceAll("[ ]+", " ").trim();
+        String[] sInList = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i=sInList.length-1; i>=0; i--){
+            builder.append(sInList[i]);
+            if(i!=0){
+                builder.append(" ");
+            }
+        }
+        return builder.toString();
     }
+
+//    private static String reverseWords(String s) {
+//        // remove leading and trailing spaces
+//        s = s.trim();
+//        // split -> divides the string and puts
+//        // elements into array based
+//        // on delimiter condition
+//        // \s+ is a regular expression for one or more spaces
+//        List<String> wordList = Arrays.asList(s.split("\\s+"));
+//        // reverses the elements of the list
+//        Collections.reverse(wordList);
+//        // returns a string separated by a space
+//        return String.join(" ", wordList);
+//    }
+
+
 }
