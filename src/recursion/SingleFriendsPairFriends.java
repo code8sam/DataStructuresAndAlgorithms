371dbbee34787a0c432917496ee7ca6df3a99080
code8sam <work.social.satyam@gmail.com>
2024-01-08 23:32:28 +0530

    leetcode : single friends pair friends

--- src/recursion/SingleFriendsPairFriends.java
+++ src/recursion/SingleFriendsPairFriends.java
@@ -8,9 +8,7 @@ public class SingleFriendsPairFriends {
     }
 
     private static int f(int n){
-        if(n==1 || n==2){
-            return n;
-        }
+        if(n==1 || n==2) return n;
         return f(n-1)+(n-1)*f(n-2);
     }
 }
