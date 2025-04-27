184c9d45be380c61e131af51a6cf963fd06d294e
code8sam <work.social.satyam@gmail.com>
2025-04-27 13:53:37 +0530

    optimized approach : second largest in array

--- src/Arrays/SecondLargestElement.java
+++ src/Arrays/SecondLargestElement.java
@@ -38,4 +38,20 @@ public class SecondLargestElement {
 
         return secondLargest==Integer.MIN_VALUE ? -1 : secondLargest;
     }
+
+    public int secondLargestElementOptimised(int[] nums) {
+        if(nums.length == 1) return -1;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>largest){
+                secondLargest = largest;
+                largest = nums[i];
+            }else if(nums[i]>secondLargest && nums[i]!=largest){
+                secondLargest = nums[i];
+            }
+        }
+        return secondLargest==Integer.MIN_VALUE ? -1 : secondLargest;
+    }
 }
