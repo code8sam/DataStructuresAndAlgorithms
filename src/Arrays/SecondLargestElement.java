5095ddd21513b3cedba6d9fb2462569b824192a1
code8sam <work.social.satyam@gmail.com>
2025-04-27 11:11:12 +0530

    better appoach : second largest element

--- src/Arrays/SecondLargestElement.java
+++ src/Arrays/SecondLargestElement.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class SecondLargestElement {
     public int secondLargestElementBruteForce(int[] nums) {
         if(nums.length == 1) return -1;
@@ -15,5 +17,25 @@ public class SecondLargestElement {
             }
         }
         return secondLargest;
+    }
+
+    public int secondLargestElementBetterApproach(int[] nums) {
+        if(nums.length == 1) return -1;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>largest){
+                largest = nums[i];
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>secondLargest && nums[i]!=largest){
+                secondLargest=nums[i];
+            }
+        }
+
+        return secondLargest==Integer.MIN_VALUE ? -1 : secondLargest;
     }
 }
